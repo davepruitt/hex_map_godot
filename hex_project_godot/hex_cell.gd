013b44987e087e74fb914283ca09c70bb3f03b79
David <15479248+davepruitt@users.noreply.github.com>
2024-09-18 05:54:52 -0600

    Beginning the work of adding support for rivers. This code is everything through section 1.2 of tutorial 6.

--- hex_project_godot/hex_cell.gd
+++ hex_project_godot/hex_cell.gd
@@ -15,6 +15,14 @@ var _hex_color: Color = Color.BLACK
 ## This is the elevation of the hex cell
 var _elevation: int = -32767
 
+var _has_incoming_river: bool = false
+
+var _has_outgoing_river: bool = false
+
+var _incoming_river_direction: HexDirectionsClass.HexDirections = HexDirectionsClass.HexDirections.NE
+
+var _outgoing_river_direction: HexDirectionsClass.HexDirections = HexDirectionsClass.HexDirections.NE
+
 #endregion
 
 #region Public data members
@@ -64,6 +72,36 @@ var hex_color: Color:
 		
 		_refresh()
 
+## This is a property indicating whether this cell has an incoming river
+var has_incoming_river: bool:
+	get:
+		return _has_incoming_river
+
+## This is a property indicating whether this cell has an outgoing river
+var has_outgoing_river: bool:
+	get:
+		return _has_outgoing_river
+
+## This is a property indicating the direction of the incoming river, if it exists
+var incoming_river_direction: HexDirectionsClass.HexDirections:
+	get:
+		return _incoming_river_direction
+
+## This is a property indicating the direction of the outgoing river, if it exists
+var outgoing_river_direction: HexDirectionsClass.HexDirections:
+	get:
+		return _outgoing_river_direction
+		
+## This property indicates whether a river exists in this cell
+var has_river: bool:
+	get:
+		return (_has_incoming_river or _has_outgoing_river)
+
+## This property indicates whether this cell contains the beginning or end of a river
+var has_river_beginning_or_end: bool:
+	get:
+		return (_has_incoming_river != _has_outgoing_river)
+
 #endregion
 
 #region Overrides
@@ -103,9 +141,63 @@ func show_ui_label (should_show_ui: bool) -> void:
 	else:
 		position_label.visible = false
 
+func has_river_through_edge (direction: HexDirectionsClass.HexDirections) -> bool:
+	var condition_01: bool = _has_incoming_river and _incoming_river_direction == direction
+	var condition_02: bool = _has_outgoing_river and _outgoing_river_direction == direction
+	return (condition_01 or condition_02)
+	
+func remove_outgoing_river () -> void:
+	#Return immediately if no outgoing river exists
+	if (not _has_outgoing_river):
+		return
+	
+	#Set the outgoing river flag to false
+	_has_outgoing_river = false
+	
+	#Refresh this cell
+	_refresh_self_only()
+	
+	#Get the neighbor in the direction the river flows
+	var neighbor: HexCell = get_neighbor(_outgoing_river_direction)
+	
+	#Set the incoming river flag on the neighbor cell
+	neighbor._has_incoming_river = false
+	
+	#Refresh the neighbor cell
+	neighbor._refresh_self_only()
+	
+func remove_incoming_river () -> void:
+	#Return immediately if no incoming river exists
+	if (not _has_incoming_river):
+		return
+		
+	#Set the incoming river flag to false
+	_has_incoming_river = false
+	
+	#Refresh this cell
+	_refresh_self_only()
+	
+	#Get the neighbor in the direction the river flows
+	var neighbor: HexCell = get_neighbor(_incoming_river_direction)
+	
+	#Set the outgoing river flag on the neighbor cell
+	neighbor._has_outgoing_river = false
+	
+	#Refresh the neighbor
+	neighbor._refresh_self_only()
+
+func remove_river () -> void:
+	remove_outgoing_river()
+	remove_incoming_river()
+	
+
 #endregion
 
 #region Private methods
+
+func _refresh_self_only () -> void:
+	if (hex_chunk):
+		hex_chunk.request_refresh()
 
 func _refresh () -> void:
 	if (hex_chunk):
