277538fc3d8ab20851660365d8572befb3141580
David <15479248+davepruitt@users.noreply.github.com>
2024-11-05 15:19:18 -0700

    Completed through section 1.7 of tutorial 20. Fixed issues with the hex terrain shader. Added global shader parameters through the Godot editor, made sure values are being set properly.

--- hex_project_godot/scripts/hex_cell_shader_data.gd
+++ hex_project_godot/scripts/hex_cell_shader_data.gd
@@ -7,6 +7,8 @@ var _cell_texture: ImageTexture = null
 
 var _cell_texture_data: PackedColorArray
 
+var _requires_update: bool = false
+
 #endregion
 
 #region Constructor
@@ -19,33 +21,47 @@ func _init() -> void:
 #region Public methods
 
 func initialize (x: int, z: int) -> void:
-	if (_cell_texture and _cell_texture_source_image):
-		_cell_texture_source_image.resize(x, z)
-		_cell_texture = ImageTexture.create_from_image(_cell_texture_source_image)
-	else:
-		_cell_texture_source_image = Image.create_empty(x, z, false, Image.FORMAT_RGBA8)	
-		_cell_texture = ImageTexture.create_from_image(_cell_texture_source_image)
-		
-		RenderingServer.global_shader_parameter_set("_HexCellData", _cell_texture)
-	
+	#Calculate the texel size
 	var texel_size: Vector4 = Vector4(1.0 / float(x), 1.0 / float(z), x, z)
-	RenderingServer.global_shader_parameter_set("_HexCellData_TexelSize", texel_size)
 	
+	#Set the size of the texture data color array
 	if (len(_cell_texture_data) != (x * z)):
 		_cell_texture_data.resize(x * z)
-	else:
-		for i in range(0, len(_cell_texture_data)):
-			_cell_texture_data[i] = Color(0, 0, 0, 0)
+	
+	#Initialize each color to all zeros
+	#In this case, the color object is being used to store NON-COLOR information
+	#Each color object in the array represents a cell in the hex grid
+	#The final element of each color object (the last 4 bytes) will store the terrain index of the cell
+	for i in range(0, len(_cell_texture_data)):
+		_cell_texture_data[i] = Color(0, 0, 0, 0)
+	
+	#Create an empty source image
+	_cell_texture_source_image = Image.create_empty(x, z, false, Image.FORMAT_RGBAF)	
+	
+	#Create the texture object from the source image
+	_cell_texture = ImageTexture.create_from_image(_cell_texture_source_image)
+	
+	#Set the global shader parameters
+	RenderingServer.global_shader_parameter_set("_HexCellData", _cell_texture)
+	RenderingServer.global_shader_parameter_set("_HexCellData_TexelSize", texel_size)
+	
+	#Set a flag indicating that an update is required
+	_requires_update = true
 
 func refresh_terrain (cell: HexCell) -> void:
-	_cell_texture_data[cell.index].a8 = cell.terrain_type_index
+	_cell_texture_data[cell.index].a = cell.terrain_type_index
+	_requires_update = true
 
 func late_update () -> void:
-	var x: int = _cell_texture_source_image.get_width()
-	var z: int = _cell_texture_source_image.get_height()
-	
-	_cell_texture_source_image.set_data(x, z, false, Image.FORMAT_RGBA8, _cell_texture_data.to_byte_array())
-	_cell_texture.update(_cell_texture_source_image)
+	if _requires_update:
+		_requires_update = false
+		
+		var x: int = _cell_texture_source_image.get_width()
+		var z: int = _cell_texture_source_image.get_height()
+		
+		var temp_byte_array: PackedByteArray = _cell_texture_data.to_byte_array()
+		_cell_texture_source_image.set_data(x, z, false, Image.FORMAT_RGBAF, temp_byte_array)
+		_cell_texture.update(_cell_texture_source_image)
 	
 
 #endregion
