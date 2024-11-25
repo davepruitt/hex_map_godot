54d3384352ed3b6614c97aa507ebc67efadad398
David <15479248+davepruitt@users.noreply.github.com>
2024-11-25 14:49:49 -0700

    Completed tutorial 24

--- hex_project_godot/scripts/hex_map_generator.gd
+++ hex_project_godot/scripts/hex_map_generator.gd
@@ -1,5 +1,15 @@
 class_name HexMapGenerator
 
+#region Private class accessible only to the map generator
+
+class MapRegion:
+	var x_min: int = 0
+	var x_max: int = 0
+	var z_min: int = 0
+	var z_max: int = 0
+
+#endregion
+
 #region Private data members
 
 var _rng = RandomNumberGenerator.new()
@@ -8,6 +18,8 @@ var _cell_count: int = 0
 
 var _search_frontier: HexCellPriorityQueue = HexCellPriorityQueue.new()
 var _search_frontier_phase: int = 0
+
+var _regions: Array[MapRegion] = []
 
 #endregion
 
@@ -40,6 +52,21 @@ var elevation_minimum: int = -2
 @export_range(6, 10)
 var elevation_maximum: int = 8
 
+@export_range(0, 10)
+var map_border_x: int = 5
+
+@export_range(0, 10)
+var map_border_z: int = 5
+
+@export_range(0, 10)
+var region_border: int = 5
+
+@export_range(1, 4)
+var region_count: int = 3
+
+@export_range(0, 100)
+var erosion_percentage: int = 50
+
 #endregion
 
 #region Public data members
@@ -63,9 +90,11 @@ func generate_map (x: int, z: int) -> void:
 	#Set the water level for the map
 	for i in range(0, _cell_count):
 		hex_grid.get_cell_from_index(i).water_level = water_level
-	
+		
 	#Raise some terrain
+	_create_regions()
 	_create_land()
+	_erode_land()
 	_set_terrain_type()
 	
 	#Reset the search phases of all cells to 0
@@ -76,20 +105,130 @@ func generate_map (x: int, z: int) -> void:
 
 #region Private methods
 
-func _get_random_cell () -> HexCell:
-	var random_cell_index: int = _rng.randi_range(0, _cell_count)
-	return hex_grid.get_cell_from_index(random_cell_index)
+func _get_random_cell (region: MapRegion) -> HexCell:
+	var random_cell_x: int = _rng.randi_range(region.x_min, region.x_max - 1)
+	var random_cell_z: int = _rng.randi_range(region.z_min, region.z_max - 1)
+	return hex_grid.get_cell_from_offset(random_cell_x, random_cell_z)
+
+func _create_regions () -> void:
+	if (_regions == null):
+		_regions = []
+	else:
+		_regions.clear()
+	
+	
+	match region_count:
+		2:
+			if (randf() < 0.5):
+				#Set the x/z min/max values (the borders of the terrain generation region)
+				var region: MapRegion = MapRegion.new()
+				region.x_min = map_border_x
+				region.x_max = (hex_grid.cell_count_x / 2) - region_border
+				region.z_min = map_border_z
+				region.z_max = hex_grid.cell_count_z - map_border_z
+				_regions.append(region)
+				
+				var region_02: MapRegion = MapRegion.new()
+				region_02.x_min = (hex_grid.cell_count_x / 2) + region_border
+				region_02.x_max = hex_grid.cell_count_x - map_border_x
+				region_02.z_min = map_border_z
+				region_02.z_max = hex_grid.cell_count_z - map_border_z
+				_regions.append(region_02)
+			else:
+				#Set the x/z min/max values (the borders of the terrain generation region)
+				var region: MapRegion = MapRegion.new()
+				region.x_min = map_border_x
+				region.x_max = hex_grid.cell_count_x - map_border_x
+				region.z_min = map_border_z
+				region.z_max = (hex_grid.cell_count_z / 2) - region_border
+				_regions.append(region)
+				
+				var region_02: MapRegion = MapRegion.new()
+				region.x_min = map_border_x
+				region.x_max = hex_grid.cell_count_x - map_border_x
+				region_02.z_min = (hex_grid.cell_count_z / 2) + region_border
+				region_02.z_max = hex_grid.cell_count_z - map_border_z
+				_regions.append(region_02)
+		3:
+			var region_01: MapRegion = MapRegion.new()
+			region_01.x_min = map_border_x
+			region_01.x_max = (hex_grid.cell_count_x / 3) - region_border
+			region_01.z_min = map_border_z
+			region_01.z_max = hex_grid.cell_count_z - map_border_z
+			_regions.append(region_01)
+			
+			var region_02: MapRegion = MapRegion.new()
+			region_02.x_min = (hex_grid.cell_count_x / 3) + region_border
+			region_02.x_max = (hex_grid.cell_count_x * 2 / 3) - region_border
+			region_02.z_min = map_border_z
+			region_02.z_max = hex_grid.cell_count_z - map_border_z
+			_regions.append(region_02)
+			
+			var region_03: MapRegion = MapRegion.new()
+			region_03.x_min = (hex_grid.cell_count_x * 2 / 3) + region_border
+			region_03.x_max = hex_grid.cell_count_x - map_border_x
+			region_03.z_min = map_border_z
+			region_03.z_max = hex_grid.cell_count_z - map_border_z
+			_regions.append(region_03)
+			
+		4:
+			var region_01: MapRegion = MapRegion.new()
+			region_01.x_min = map_border_x
+			region_01.x_max = (hex_grid.cell_count_x / 2) - region_border
+			region_01.z_min = map_border_z
+			region_01.z_max = (hex_grid.cell_count_z / 2) - map_border_z
+			_regions.append(region_01)
+			
+			var region_02: MapRegion = MapRegion.new()
+			region_02.x_min = hex_grid.cell_count_x / 2 + region_border
+			region_02.x_max = hex_grid.cell_count_x - map_border_x
+			region_02.z_min = map_border_z
+			region_02.z_max = (hex_grid.cell_count_z / 2) - map_border_z
+			_regions.append(region_02)
+			
+			var region_03: MapRegion = MapRegion.new()
+			region_03.x_min = hex_grid.cell_count_x / 2 + region_border
+			region_03.x_max = hex_grid.cell_count_x - map_border_x
+			region_03.z_min = hex_grid.cell_count_z / 2 + region_border
+			region_03.z_max = hex_grid.cell_count_z - map_border_z
+			_regions.append(region_03)
+			
+			var region_04: MapRegion = MapRegion.new()
+			region_04.x_min = map_border_x
+			region_04.x_max = (hex_grid.cell_count_x / 2) - region_border
+			region_04.z_min = hex_grid.cell_count_z / 2 + region_border
+			region_04.z_max = hex_grid.cell_count_z - map_border_z
+			_regions.append(region_04)
+			
+			pass
+		_:
+			var region: MapRegion = MapRegion.new()
+			region.x_min = map_border_x
+			region.x_max = hex_grid.cell_count_x - map_border_x
+			region.z_min = map_border_z
+			region.z_max = hex_grid.cell_count_z - map_border_z
+			_regions.append(region)
 
 func _create_land () -> void:
 	var land_budget: int = roundi(_cell_count * land_percentage * 0.01)
 	
-	while (land_budget > 0):
-		var chunk_size: int = _rng.randi_range(chunk_size_min, chunk_size_max + 1)
-		var random_value: float = _rng.randf()
-		if (random_value < sink_probability):
-			land_budget = _sink_terrain(chunk_size, land_budget)
-		else:
-			land_budget = _raise_terrain(chunk_size, land_budget)
+	for guard in range(0, 10000):
+		#Generate a value indicating whether we should sink or raise land
+		var should_sink: bool = _rng.randf() < sink_probability
+		
+		#Iterate over each region
+		for i in range(0, len(_regions)):
+			var region: MapRegion = _regions[i]
+			var chunk_size: int = _rng.randi_range(chunk_size_min, chunk_size_max)
+			if (should_sink):
+				land_budget = _sink_terrain(chunk_size, land_budget, region)
+			else:
+				land_budget = _raise_terrain(chunk_size, land_budget, region)
+				if (land_budget == 0):
+					return
+		
+	if (land_budget > 0):
+		print_debug("Failed to use up " + str(land_budget) + " land budget.")
 
 func _set_terrain_type () -> void:
 	for i in range(0, _cell_count):
@@ -97,9 +236,9 @@ func _set_terrain_type () -> void:
 		if (not cell.is_underwater):
 			cell.terrain_type_index = cell.elevation - cell.water_level
 
-func _raise_terrain (chunk_size: int, budget: int) -> int:
+func _raise_terrain (chunk_size: int, budget: int, region: MapRegion) -> int:
 	_search_frontier_phase += 1
-	var first_cell: HexCell = _get_random_cell()
+	var first_cell: HexCell = _get_random_cell(region)
 	first_cell.search_phase = _search_frontier_phase
 	first_cell.distance = 0
 	first_cell.search_heuristic = 0
@@ -141,9 +280,9 @@ func _raise_terrain (chunk_size: int, budget: int) -> int:
 	
 	return budget
 
-func _sink_terrain (chunk_size: int, budget: int) -> int:
+func _sink_terrain (chunk_size: int, budget: int, region: MapRegion) -> int:
 	_search_frontier_phase += 1
-	var first_cell: HexCell = _get_random_cell()
+	var first_cell: HexCell = _get_random_cell(region)
 	first_cell.search_phase = _search_frontier_phase
 	first_cell.distance = 0
 	first_cell.search_heuristic = 0
@@ -182,5 +321,68 @@ func _sink_terrain (chunk_size: int, budget: int) -> int:
 	_search_frontier.clear()
 	
 	return budget
+
+func _erode_land () -> void:
+	var erodible_cells: Array[HexCell] = []
+	
+	for i in range(0, _cell_count):
+		var cell: HexCell = hex_grid.get_cell_from_index(i)
+		if (_is_erodible(cell)):
+			erodible_cells.append(cell)
+	
+	var target_erodible_count: int = int(len(erodible_cells) * (100 - erosion_percentage) * 0.01)
+	
+	while (len(erodible_cells) > target_erodible_count):
+		var index: int = _rng.randi_range(0, len(erodible_cells) - 1)
+		var cell: HexCell = erodible_cells[index]
+		var target_cell: HexCell = _get_erosion_target(cell)
+		
+		cell.elevation -= 1
+		target_cell.elevation += 1
+		
+		if (not _is_erodible(cell)):
+			erodible_cells.erase(cell)
+	
+		for d in range(0, 6):
+			var neighbor: HexCell = cell.get_neighbor(d)
+			if ((neighbor) and 
+				(neighbor.elevation == cell.elevation + 2) and
+				(neighbor not in erodible_cells)):
+				
+				erodible_cells.append(neighbor)
+		
+		if (_is_erodible(target_cell) and (target_cell not in erodible_cells)):
+			erodible_cells.append(target_cell)
+		
+		for d in range(0, 6):
+			var neighbor: HexCell = target_cell.get_neighbor(d)
+			if ((neighbor) and 
+				(neighbor != cell) and 
+				(neighbor.elevation == (target_cell.elevation + 1)) and 
+				(not _is_erodible(neighbor))):
+				
+				erodible_cells.erase(neighbor)
+	
+
+func _is_erodible (cell: HexCell) -> bool:
+	var erodible_elevation: int = cell.elevation - 2
+	for d in range(0, 6):
+		var neighbor: HexCell = cell.get_neighbor(d)
+		if (neighbor) and (neighbor.elevation <= erodible_elevation):
+			return true
+	
+	return false
+
+func _get_erosion_target (cell: HexCell) -> HexCell:
+	var candidates: Array[HexCell] = []
+	var erodible_elevation: int = cell.elevation - 2
+	
+	for d in range(0, 6):
+		var neighbor: HexCell = cell.get_neighbor(d)
+		if (neighbor) and (neighbor.elevation <= erodible_elevation):
+			candidates.append(neighbor)
+	
+	var target: HexCell = candidates[_rng.randi_range(0, len(candidates) - 1)]
+	return target
 
 #endregion
